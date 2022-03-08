4cb84164423adc0c5be35393de440153b971a695
MMartina020 <marcsikaa15@gmail.com>
2022-03-08 08:22:13 +0100

    tesztelési esetek

--- sebesseg.js
+++ sebesseg.js
@@ -1,5 +1,18 @@
-function sebesseg(){
-    return 0;
+/*
+@author: MM 
+ut - pozitiv valós
+ido - pozitiv valós
+return 
+    - sebesseg nem negatív 
+    - hibás paraméterek esetén : 'err' stringet ad vissza 
+
+
+*/
+function sebesseg( ut, ido){
+    if( ido <= 0 || ut < 0){
+        return 'err';
+    }
+    return ut/ido;
 }
 
 module.exports = sebesseg;
--- sebesseg.test.js
+++ sebesseg.test.js
@@ -1,10 +1,22 @@
-const sebesseg = require('sebesseg');
+const sebesseg = require('./sebesseg');
 
+/*
+
+*/
 test('az ut 2m az ido 2sec, akkor a sebesseg 1m/s', () => {
     expect(sebesseg(2, 2)).toBe(1);
     //ide lehet bővíteni
   });
 
   test('az ut 2m az ido 0sec, akkor hibát kell kapjunk', () => {
-    expect(sebesseg(2, 0)).toBe('err');
+    expect(sebesseg(2, 0)).toBe('err'); //then
+  });
+
+  test('az ut 2m az ido negativ, akkor hibát kell kapjunk', () => {
+    expect(sebesseg(2, -3)).toBe('err'); 
+    expect(sebesseg(10, -1)).toBe('err'); 
+  });
+
+  test('az ut 0m az ido pozitiv, akkor hibát kell kapjunk', () => {
+    expect(sebesseg(0, 1)).toBe(0); 
   });
